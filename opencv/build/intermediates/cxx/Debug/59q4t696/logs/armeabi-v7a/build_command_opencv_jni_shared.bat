0b93bbf4e92d4d788d774a20f5d8760cff0577f1
MuhammadAsif <masif.bscsses21@iba-suk.edu.pk>
2024-09-28 18:32:37 +0500

    added model.tflite in assets and dependencies of the mlkit and cameraX

--- opencv/build/intermediates/cxx/Debug/59q4t696/logs/armeabi-v7a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\PMLS\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\PMLS\\Desktop\\CompanionEK\\opencv\\.cxx\\Debug\\59q4t696\\armeabi-v7a" ^
-  opencv_jni_shared
