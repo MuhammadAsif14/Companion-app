cb9e55038f1834050333280709f0136564b1b27a
MuhammadAsif <masif.bscsses21@iba-suk.edu.pk>
2024-09-28 17:47:38 +0500

    added model.tflite in assets and dependencies of the mlkit and cameraX

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,9 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+    buildFeatures {
+        viewBinding= true
+    }
 
     buildTypes {
         release {
@@ -35,27 +38,50 @@ android {
     }
 }
 
-
 dependencies {
-
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
     // RecyclerView dependency
     implementation("androidx.recyclerview:recyclerview:1.3.2")
 
     // Coroutines dependency
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
 
-    //charts
-   implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    // MPAndroidChart library for charts
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
+    // TensorFlow Lite for on-device machine learning
+    implementation("org.tensorflow:tensorflow-lite:2.16.1") // Check for updates
+//
+//    // CameraX dependencies for camera access
+//    implementation("androidx.camera:camera-core:1.3.4")
+//    implementation("androidx.camera:camera-camera2:1.3.4")
+//    implementation("androidx.camera:camera-lifecycle:1.3.4")
+//    implementation("androidx.camera:camera-view:1.3.4") // Use stable version
+////    implementation(":opencv")
+    // Face Detection
+    implementation("com.google.mlkit:face-detection:16.1.5")
+
+// CameraX
+    var camerax_version = "1.3.0-alpha04"
+    implementation("androidx.camera:camera-core:${camerax_version}")
+    implementation("androidx.camera:camera-camera2:${camerax_version}")
+    implementation("androidx.camera:camera-lifecycle:${camerax_version}")
+    implementation("androidx.camera:camera-video:${camerax_version}")
+    implementation("androidx.camera:camera-view:${camerax_version}")
+    implementation("androidx.camera:camera-mlkit-vision:${camerax_version}")
+    implementation("androidx.camera:camera-extensions:${camerax_version}")
+
+// CameraSource
+    implementation ("com.google.android.gms:play-services-vision-common:19.1.3")
+
 
 
 }
---
+++ opencv/build/intermediates/cxx/Debug/59q4t696/logs/x86_64/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\PMLS\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\PMLS\\Desktop\\CompanionEK\\opencv\\.cxx\\Debug\\59q4t696\\x86_64" ^
+  opencv_jni_shared
